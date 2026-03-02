8f1ee5326d1f1d191acceacf07f76e47a013cdd8
Sergio Cañas <s4c0205@gmail.com>
2026-03-02 09:05:16 -0500

    Agregar softDelete a clientes

--- database/migrations/2026_02_27_190710_create_clients_table.php
+++ database/migrations/2026_02_27_190710_create_clients_table.php
@@ -20,6 +20,8 @@ return new class extends Migration
             $table->string('address');
             $table->string('phone');
             $table->timestamps();
+
+        $table->softDeletes();
         });
     }
 
